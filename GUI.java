68003ef378686a9e1ca728a4f13a84e56bba50a2
tosia <todospial@gmail.com>
2024-05-06 19:03:59 +0200

    zmieniono czcionki

--- GUI.java
+++ GUI.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Font;
+import java.io.File;
+import java.io.IOException;
 
 public class GUI extends JFrame {
 
@@ -16,54 +19,85 @@ public class GUI extends JFrame {
     JButton znajdz_sciezke;
     JButton zmien_pkt;
 
+
     GUI() {
         setTitle("Labirynt");
         setSize(900, 1000);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
+        Font syne = null;
+        
+        try{
+            syne = Font.createFont(Font.TRUETYPE_FONT, new File("Syne-ExtraBold.ttf")).deriveFont(35f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("Syne-ExtraBold.ttf")));
+        }catch (IOException | FontFormatException e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+        Font poppins = null;
+
+        try{
+            poppins = Font.createFont(Font.TRUETYPE_FONT, new File("Poppins-Medium.ttf")).deriveFont(13f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("Poppins-Medium.ttf")));
+        }catch (IOException | FontFormatException e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
 
         tytul = new JLabel("Rozwiązywacz labiryntu", SwingConstants.CENTER);
-        tytul.setFont(new Font("Arial", Font.BOLD, 36));
-        tytul.setBounds(200, 50, 500, 60);
+        //tytul.setFont(new Font("Arial", Font.BOLD, 36));
+        tytul.setFont(syne);
+        tytul.setBounds(-60, 50, 1000, 60);
         add(tytul);
 
         JPanel panel = new JPanel();
         panel.setLayout(null);
         panel.setBackground(Color.WHITE);
 
+
         plik_nazwa = new JLabel("Podaj nazwę pliku z labiryntem:");
         plik_nazwa.setBounds(100, 150, 300, 30);
+        plik_nazwa.setFont(poppins);
         panel.add(plik_nazwa);
 
         nazwa = new JTextField();
         nazwa.setBounds(400, 150, 300, 30);
+        nazwa.setFont(poppins);
         panel.add(nazwa);
 
         plik_rodzaj = new JLabel("Wybierz rodzaj pliku:");
         plik_rodzaj.setBounds(100, 200, 300, 30);
+        plik_rodzaj.setFont(poppins);
         panel.add(plik_rodzaj);
 
         String[] rodzaje = {"  Plik tekstowy", "  Plik binarny"};
         wybor_rodzaju = new JComboBox<>(rodzaje);
         wybor_rodzaju.setBounds(400, 200, 300, 30);
         wybor_rodzaju.setBackground(Color.WHITE);
+        wybor_rodzaju.setFont(poppins);
         panel.add(wybor_rodzaju);
+
 
         szerokosc_lab = new JLabel("Szerokość labiryntu:");
         szerokosc_lab.setBounds(100, 250, 150, 30);
+        szerokosc_lab.setFont(poppins);
         panel.add(szerokosc_lab);
 
         szerokosc = new JTextField();
         szerokosc.setBounds(400, 250, 300, 30);
+        szerokosc.setFont(poppins);
         panel.add(szerokosc);
 
         wysokosc_lab = new JLabel("Wysokość labiryntu:");
         wysokosc_lab.setBounds(100, 300, 150, 30);
+        wysokosc_lab.setFont(poppins);
         panel.add(wysokosc_lab);
 
         wysokosc = new JTextField();
         wysokosc.setBounds(400, 300, 300, 30);
+        wysokosc.setFont(poppins);
         panel.add(wysokosc);
 
         wyswietl = new JButton("Wyświetl labirynt");
@@ -73,6 +107,7 @@ public class GUI extends JFrame {
         wyswietl.setVerticalTextPosition(JButton.CENTER);
         wyswietl.setBackground(new Color(0xAFD6D1));
         wyswietl.setBorder(BorderFactory.createEmptyBorder());
+        wyswietl.setFont(poppins);
         panel.add(wyswietl);
 
 
@@ -89,15 +124,17 @@ public class GUI extends JFrame {
         znajdz_sciezke.setVerticalTextPosition(JButton.CENTER);
         znajdz_sciezke.setBackground(new Color(0xAFD6D1));
         znajdz_sciezke.setBorder(BorderFactory.createEmptyBorder());
+        znajdz_sciezke.setFont(poppins);
         panel.add(znajdz_sciezke);
 
         zmien_pkt = new JButton("Zmień punkt startowy/końcowy");
-        zmien_pkt.setBounds(320, 780, 220, 50);
+        zmien_pkt.setBounds(320, 780, 240, 50);
         zmien_pkt.setFocusable(false);
         zmien_pkt.setHorizontalTextPosition(JButton.CENTER);
         zmien_pkt.setVerticalTextPosition(JButton.CENTER);
         zmien_pkt.setBackground(new Color(0xAFD6D1));
         zmien_pkt.setBorder(BorderFactory.createEmptyBorder());
+        zmien_pkt.setFont(poppins);
         panel.add(zmien_pkt);
 
         panel.setBounds(0, 0, 1000, 1000);
